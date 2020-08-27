9841fb0ce562b7a5eaf7036029ec14c1b0ec8f0e
Alim Nevilov <52937912+nevilov@users.noreply.github.com>
2020-08-27 22:00:27 +0300

    CMP1

--- app/src/main/java/com/example/compass/MainActivity.kt
+++ app/src/main/java/com/example/compass/MainActivity.kt
@@ -1,11 +1,52 @@
 package com.example.compass
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.RotateAnimation
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SensorEventListener {
+    private var manager: SensorManager? = null
+    private var currentDegree: Int = 0
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        manager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+    }
+
+    override fun onResume() {
+        super.onResume()
+        manager?.
+                 registerListener(this, manager?.getDefaultSensor(Sensor.TYPE_ORIENTATION),SensorManager.SENSOR_DELAY_GAME)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        manager?.
+                unregisterListener(this)
+    }
+
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+
+    }
+
+    override fun onSensorChanged(p0: SensorEvent?) {
+        val degree:Int = p0?.values?.get(0)?.toInt()!!
+        tvDegree.text = degree.toString()
+        val rotationAnimation = RotateAnimation(currentDegree.toFloat(),
+            (-degree).toFloat(), Animation.RELATIVE_TO_SELF, 0.5f,
+            Animation.RELATIVE_TO_SELF, 0.5f)
+
+        rotationAnimation.duration = 210
+        rotationAnimation.fillAfter = true
+        imDegree.startAnimation(rotationAnimation)
     }
 }
